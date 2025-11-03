bc2a6a0d234972b83757a08e88319c20bedd72fc
shreddar88 <asgeir_sverrisson@hotmail.com>
2025-11-03 11:40:28 +0100

    Update weather_alert.py

--- weather_alert.py
+++ weather_alert.py
@@ -2,6 +2,7 @@ import os
 import requests
 import smtplib
 from email.message import EmailMessage
+from datetime import datetime, timedelta
 
 API_KEY = os.getenv("OPENWEATHER_API_KEY")
 EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
