51c404285dae5fbbc6f9a3fcff47df66ca584db9
shreddar88 <asgeir_sverrisson@hotmail.com>
2025-11-03 11:38:41 +0100

    Update weather_alert.py

--- weather_alert.py
+++ weather_alert.py
@@ -14,7 +14,7 @@ URL = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appi
 #URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
 
 response = requests.get(URL)
-data = response.json(
+data = response.json()
     
 alert_needed = False
 alert_time = None
