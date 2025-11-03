9b45f8d2b769f3356655ffe5c36f8cfb8e37f51f
shreddar88 <asgeir_sverrisson@hotmail.com>
2025-11-03 11:20:41 +0100

    Update weather_alert.py

--- weather_alert.py
+++ weather_alert.py
@@ -9,25 +9,18 @@ EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 TO_EMAIL = os.getenv("TO_EMAIL")
 
 LAT, LON = 55.6050, 13.0038 # Malmö coordinates
-#RAIN_THRESHOLD = 0.3 # 30% chance
-
-#URL = f"https://api.openweathermap.org/data/3.0/onecall?lat={LAT}&lon={LON}&exclude=minutely,daily,alerts&units=metric&appid={API_KEY}"
-
 URL = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}&units=metric"
-
-
 #CITY = "Malmo,SE"
 #URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
 
 response = requests.get(URL)
 weather = response.json()
 
-
 temp = weather["main"]["temp"]
 rain = "rain" in weather
-#if temp < 0 or rain:
-if temp < 15 or rain:
-    alert = f"⚠️ Weather Alert for Malmoe: {temp}°C and rain={rain}"
+if temp < 0 or rain:
+#if temp < 15 or rain:
+    alert = f"⚠️ Temperatur i Malmö {temp}°C och sannolikhet för regn={rain}"
     msg = EmailMessage()
     msg.set_content(alert)
     msg["Subject"] = "Väder varning, snöröjargänget"
